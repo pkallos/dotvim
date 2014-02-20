1dbeb693ca274963e41e673aa368976df7ed5c02
Phil Kallos <pkallos@sugarinc.com>
2014-02-20 23:47:10 +0000

    More fussing with vundle.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 mv $HOME/.vimrc $HOME/.vimrc.`date +"%T"`.bak
 ln -nsf $DIR/.vimrc $HOME/.vimrc
 vim +BundleInstall +qall
